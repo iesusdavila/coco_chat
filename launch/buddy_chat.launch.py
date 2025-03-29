88eb3290d18d12de42c75cb12a88e31d0df53cd6
Iesus Davila <iesusdavila@gmail.com>
2025-03-29 13:30:52 -0500

    Borrar librerias sin uso

--- launch/buddy_chat.launch.py
+++ launch/buddy_chat.launch.py
@@ -1,9 +1,6 @@
-import os
 from launch import LaunchDescription
 from launch_ros.actions import LifecycleNode
 from launch_ros.actions import Node
-from launch.actions import RegisterEventHandler
-from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
