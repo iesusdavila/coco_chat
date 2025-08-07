1369c9bd0f9d4433fe5541a513c903bda944f590
Iesus Davila <iesusdavila@gmail.com>
2025-08-07 11:31:35 -0500

    React states for different agent implementations

--- scripts/utils/react_state.py
+++ scripts/utils/react_state.py
@@ -1,5 +1,13 @@
-from typing import TypedDict, Annotated
+from typing import TypedDict, Annotated, Optional
 from langgraph.graph import add_messages
 
 class ChatState(TypedDict): 
     messages: Annotated[list, add_messages]
+    movement_intent: Optional[dict]
+    robot_action_required: Optional[bool]
+
+class MovementState(TypedDict):
+    messages: Annotated[list, "add_messages"]
+    movement_detected: bool
+    movement_type: str
+    joints_to_move: dict
